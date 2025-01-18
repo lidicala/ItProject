18b38e151c2d30586fa668e827543dc50f5e8233
JG8993 <jjgomez@yahoo.com>
2025-01-18 09:50:09 -0800

    Jake updates

--- courses.js
+++ courses.js
@@ -1,31 +1,29 @@
-// Base class for all courses
-class Course 
-{
-    constructor(name, prerequisites = []) 
-    {
-        this.name = name;
+// Course Class Definition
+class Course {
+    constructor(id, prerequisites = []) {
+        this.id = id;
         this.prerequisites = prerequisites;
-        this.isTaken = false;
-        this.status = "not-eligible";
-    }
-
-    // Check if the course is eligible 
-    isEligible(takenCourses) 
-    {
-
-    }
-
-    // Mark the course as taken
-    markAsTaken() 
-    {
-        this.isTaken = true;
-        this.status = "taken";
+        this.taken = false;
     }
 }
 
-// Class to manage taken classes
-class Progress
-{
-
-}
+// Initialize courses
+const courses = [
+    new Course("csd110"),
+    new Course("csd112"),
+    new Course("engl101"),
+    new Course("cs141", ["csd110"]),
+    new Course("csd122", ["csd110", "csd112"]),
+    new Course("csd138", ["csd110"]),
+    new Course("cs143", ["cs141"]),
+    new Course("csd268", ["cs141"]),
+    new Course("csd230", ["cs143 or csd228"]),
+    new Course("csd275", ["csd110", "csd112"]),
+    new Course("csd233", ["cs143"]),
+    new Course("csd228", ["cs141"]),
+    new Course("csd298", ["cs143"]),
+    new Course("dsgn290", ["art102"]),
+    new Course("csd297", ["csd112", "csd122", "csd138", "cs141", "csd228 or csd268"]),
+    new Course("csd235", ["cs143"]),
+];
 
